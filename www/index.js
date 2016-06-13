904aacd1bb040e0229be63ea4c0a4052bedaff1d
freeman <the.freeman.lab@gmail.com>
2016-06-12 21:34:22 -0700

    add examples

--- www/index.js
+++ www/index.js
@@ -6,7 +6,9 @@ app.model({
   namespace: 'examples',
   state: { list: [
     'basic', 'batch', 'bunny', 'camera', 'dds', 'dynamic', 
-    'elements', 'envmap', 'feedback', 'geomorph'
+    'elements', 'envmap', 'feedback', 'geomorph', 'life',
+    'lighting', 'mipmap', 'particles', 'scope', 'text',
+    'texture', 'theta360', 'tile', 'video'
     ] 
   }
 })
